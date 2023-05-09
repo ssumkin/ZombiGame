9ecfbd9330355f88bd8498badd4fdc4ced81c99c
ssumkin <rms856@gmail.com>
2023-05-09 23:00:30 +0900

    update

--- src/ZombiGame.java
+++ src/ZombiGame.java
@@ -59,76 +59,76 @@
  
  
      public static void main(String[] args) {
-         Scanner scanner = new Scanner(System.in);
-         ZombiGame zombiGame = new ZombiGame();
-         Random rand = new Random();
- 
- 
-         int[] map = new int[20];
-         int hero = 1;
-         int zom1 = 2, zom2 = 3;
-         int heroL = 0;
-         int zom1L = 9;
-         int zom2L = 17;
-         boolean gameLoop = true;
- 
- 
-         
-         map[heroL] = hero;
-         map[zom1L] = zom1;
-         map[zom2L] = zom2;
- 
-         for (int i : map) {
-             System.out.print(i);
-         } 
-         System.out.println();
- 
-         while(gameLoop) {
-             System.out.print(">> ");
-             int move = scanner.nextInt();
- 
-             if(heroL == 1) {
-                 if(move == 2) {
-                     continue;
-                 }
-             }
- 
-             switch(move) {
-                 case 1: {
-                     heroL = zombiGame.heroLocation(map);
-                     map[heroL] = 0;
-                     map[heroL+1] = hero;
-                     break;
-                 } 
-                 case 2: {
-                     heroL = zombiGame.heroLocation(map);
-                     map[heroL] = 0;
-                     map[heroL-1] = hero;
-                     break; 
-                 }
-                 case 3: {
-                     heroL = zombiGame.heroLocation(map);
-                     map[heroL] = 0;
-                     map[heroL+2] = hero;
-                     break; 
-                 }
-                 default: {
-                     System.out.println("다시 입력해주세요.");
-                     break;
-                 } 
-             }
- 
-             if(zombiGame.heroLocation(map) == zombiGame.zom1Location(map)) {
-                 for (int i : map) {
-                     System.out.print(i);
-                 } 
-             }
- 
-  
-             for (int i : map) {
-                 System.out.print(i);
-             } 
-             System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        ZombiGame zombiGame = new ZombiGame();
+        Random rand = new Random();
+
+    
+        int[] map = new int[20];
+        int hero = 1;
+        int zom1 = 2, zom2 = 3;
+        int heroL = 0;
+        int zom1L = 9;
+        int zom2L = 17;
+        boolean gameLoop = true;
+
+
+        
+        map[heroL] = hero;
+        map[zom1L] = zom1;
+        map[zom2L] = zom2;
+
+        for (int i : map) {
+            System.out.print(i);
+        } 
+        System.out.println();
+
+        while(gameLoop) {
+            System.out.print(">> ");
+            int move = scanner.nextInt();
+
+            if(heroL == 1) {
+                if(move == 2) {
+                    continue;
+                }
+            }
+
+            switch(move) {
+                case 1: {
+                    heroL = zombiGame.heroLocation(map);
+                    map[heroL] = 0;
+                    map[heroL+1] = hero;
+                    break;
+                } 
+                case 2: {
+                    heroL = zombiGame.heroLocation(map);
+                    map[heroL] = 0;
+                    map[heroL-1] = hero;
+                    break; 
+                }
+                case 3: {
+                    heroL = zombiGame.heroLocation(map);
+                    map[heroL] = 0;
+                    map[heroL+2] = hero;
+                    break; 
+                }
+                default: {
+                    System.out.println("다시 입력해주세요.");
+                    break;
+                } 
+            }
+
+            if(zombiGame.heroLocation(map) == zombiGame.zom1Location(map)) {
+                for (int i : map) {
+                    System.out.print(i);
+                } 
+            }
+
+
+            for (int i : map) {
+                System.out.print(i);
+            } 
+            System.out.println();
  
  
              
